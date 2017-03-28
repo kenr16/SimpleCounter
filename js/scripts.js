b674157753d311d0e3bfd981c5af6ef70c1e4b9b
James Higgins and Ken Rutan <james@>
2017-03-28 10:40:13 -0700

    Add new functionality to the third function that replaces vowels.

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,5 @@
 $(document).ready(function() {
 
-
-
-
     $("#formOne").submit(function(event) {
       alert("Submission Success!");
       var endNumber = parseFloat($("input#endNum").val());
@@ -12,11 +9,13 @@ $(document).ready(function() {
 
       if (endNumber != Number || stepNumber != Number || startNumber != Number) {
         alert("Please enter a number!");
-        return false;
+        return
       } else if (endnumber < 0 || stepNumber < 0 || startNumber < 0) {
         alert("Please enter a positive number!");
+        return
       } else if (stepNumber+startNumber > endNumber) {
         alert("Warning the step is larger than the end!")
+        return
       } else {
         for (var counter=startNumber; counter <= endNumber; counter+=stepNumber) {
           alert(counter);
@@ -35,13 +34,39 @@ $(document).ready(function() {
       }//
 
       var numbers = [1, 2, 3, 4];
-        for(var a=0; a<numbers.length; a++ ) {
-          alert(numbers[a]*2);
-
-        }
-
-
-
+      for(var a=0; a<numbers.length; a++ ) {
+        alert(numbers[a]*2);
+      }
     event.preventDefault();
+    });
+
+    // *******************************
+    // Back End Logic
+    // *******************************
+    var vowelReplace = function(sentInt) {
+      var outputString = ""
+      for (var b=0; b<sentInt.length; b++) {
+        var letter = sentInt[b];
+        if(letter == "a" ||
+           letter == "e" ||
+           letter == "i" ||
+           letter == "o" ||
+           letter == "u"){
+             outputString = outputString + "-";
+        } else {
+          outputString = outputString + sentInt[b];
+        }
+      }
+        return outputString
+    };
+
+    // *******************************
+    // Front End Logic
+    // *******************************
+    $("#formThree").submit(function(event) {
+      var sentenceInput = $("input#sentIn").val();
+      //var finalString = sentenceInput.replace(/[a,e,i,o,u]/gi, "-");
+      alert(vowelReplace(sentenceInput));
+      event.preventDefault();
     });
 });
